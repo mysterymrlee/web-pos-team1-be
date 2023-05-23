7ffa161c7239187468ff4cab750df6bd990751db
jinaa11 <jinaa11@gmail.com>
2023-05-23 20:11:59 +0900

    feat: 선물하기 controller 구현

--- src/main/java/com/ssg/webpos/controller/GiftController.java
+++ src/main/java/com/ssg/webpos/controller/GiftController.java
@@ -1,9 +1,14 @@
 package com.ssg.webpos.controller;
 
+import com.ssg.webpos.dto.GiftRequestDTO;
 import com.ssg.webpos.repository.delivery.DeliveryRedisImplRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/gift")
@@ -11,5 +16,25 @@ public class GiftController {
   @Autowired
   DeliveryRedisImplRepository deliveryRedisImplRepository;
 
+  @PostMapping("/add")
+  public ResponseEntity saveGiftInfo(@RequestBody GiftRequestDTO giftRequestDTO) {
+    try {
+      deliveryRedisImplRepository.saveGiftRecipientInfo(giftRequestDTO);
+      return new ResponseEntity(HttpStatus.CREATED);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @GetMapping("")
+  public ResponseEntity getGiftInfo() {
+    try {
+      Map<String, Map<String, List<Object>>> all = deliveryRedisImplRepository.findAll();
+      return new ResponseEntity(all, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+  }
 
 }
