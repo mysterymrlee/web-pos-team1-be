4a21149cdf77138d24e2c24b85a9f38c816dc44d
mysterymrlee <100182498+mysterymrlee@users.noreply.github.com>
2023-06-09 18:52:48 +0900

    feat:modify orderDetailResponseDTO

--- src/main/java/com/ssg/webpos/controller/admin/BranchAdminManagerController.java
+++ src/main/java/com/ssg/webpos/controller/admin/BranchAdminManagerController.java
@@ -50,59 +50,7 @@ public class BranchAdminManagerController {
     private final StockReportRepository stockReportRepository;
     private final CartRepository cartRepository;
     private final PointUseHistoryRepository pointUseHistoryRepository;
-    //일별정산내역 조회
-    //RequestSettlementMonthDTO로 String 타입의 year(ex. "2023")을 받으면 parse 활용해서 2023년도의 월별정산내역 조회
-    // 받아오는 건 String으로 받아오고 스프링에서 startDate, endDate를 만들어서 레포지토리 매서드로 활용
-    // 로그인 구현시 해당 store_id 만 나오도록 변경
-//    @PostMapping("/settlement-month")
-//    public ResponseEntity settlementMonth(@RequestBody RequsestSettlementMonthDTO requestSettlementMonthDTO) {
-//        try {
-//            String year = requestSettlementMonthDTO.getYear();
-//            List<SettlementMonth> settlementMonths = settlementMonthService.selectByYear(year);
-//            List<SettlementMonthReportDTO> reportDTOs = new ArrayList<>();
-//
-//            for(SettlementMonth settlementMonth:settlementMonths) {
-//                SettlementMonthReportDTO reportDTO = new SettlementMonthReportDTO();
-//                reportDTO.setSettlementMontnId(settlementMonth.getId());
-//                reportDTO.setSettlementPrice(settlementMonth.getSettlementPrice());
-//                reportDTO.setSettlementDate(settlementMonth.getSettlementDate());
-//                reportDTO.setStoreId(settlementMonth.getStore().getId());
-//                reportDTO.setStoreName(settlementMonth.getStore().getName());
-//                reportDTO.setCreatedDate(settlementMonth.getCreatedDate());
-//                reportDTOs.add(reportDTO);
-//            }
-//
-//            return new ResponseEntity(reportDTOs, HttpStatus.OK);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-//        }
-//    }
 
-    //year 조회, 보이는 DTO : 입력한 year에 해당하는 settlement_month 정산내역
-    //demo version
-
-//    @PostMapping("/request-param/year/settlement-month")
-//    public List<SettlementMonthReportDTO> settlementMonthByYear(@RequestBody RequestYearDTO requestYearDTO) {
-//        try {
-//            int yearValue = Integer.parseInt(requestYearDTO.getYear());
-//            // order_date에 year가 있는 row를 조회
-//            List<SettlementMonth> settlementMonths = settlementMonthRepository.findBySettlementDateContaining(String.valueOf(yearValue));
-//
-//            // 조회 결과를 DTO로 변환
-//            List<SettlementMonthReportDTO> reportDTOList = settlementMonths.stream()
-//                    .map(settlementMonth -> new SettlementMonthReportDTO(settlementMonth))
-//                    .collect(Collectors.toList());
-//
-//            return reportDTOList;
-//        } catch (Exception e) {
-//            // 예외 처리
-//            e.printStackTrace();
-//            // 빈 리스트 반환 또는 예외 처리에 따라 적절한 동작 수행
-//            return Collections.emptyList();
-//        }
-//
-//    }
 
     //조건을 명시한 상태에서 settlement_day 내역 GET (조건 : store_id=1L, settlement_date="2023-05-08")
     @GetMapping("/settlementDay-constant-date")
@@ -161,90 +109,6 @@ public class BranchAdminManagerController {
         }
 
     }
-
-    // 특정 기간 일별 내역 조회
-//    @GetMapping("/settlementDay-variable-range")
-//    public List<SettlementDayReportDTO> settlementDayByVariableRange(@RequestParam("startDate") String startDate,
-//                                              @RequestParam("endDate") String endDate) throws DateTimeParseException {
-//        try {
-//            List<SettlementDay> settlementDays = settlementDayService.selectByStoreIdAndDayBetween(1L,startDate,endDate);
-//            List<SettlementDayReportDTO> reportDTOs = new ArrayList<>();
-//
-//            for (SettlementDay settlementDay : settlementDays) {
-//                SettlementDayReportDTO reportDTO = new SettlementDayReportDTO();
-//                reportDTO.setSettlementDayId(settlementDay.getId());
-//                reportDTO.setSettlementPrice(settlementDay.getSettlementPrice());
-//                reportDTO.setSettlementDate(settlementDay.getSettlementDate());
-//                reportDTO.setStoreId(settlementDay.getStore().getId());
-//                reportDTO.setStoreName(settlementDay.getStore().getName());
-//                reportDTO.setCreatedDate(settlementDay.getCreatedDate());
-//                reportDTOs.add(reportDTO);
-//            }
-//
-//            return reportDTOs;
-//        } catch (Exception e) {
-//            return Collections.emptyList();
-//        }
-//    }
-
-    // 월별 내역 조회
-    // 날짜를 req 받는 상태에서 settlement_day 내역 GET
-    // 예시 URL : http://localhost:8080/api/v1/manager/test3?settlementDate=2023-05-08
-    // next level : 점장 로그인시 점장의 해당 store_id 정산내역 표시
-//    @GetMapping("/settlementMonth-variable-date-yyyy-MM")
-//    public List<SettlementMonthReportDTO> settlementMonthByVariableDateyyyyMM(@RequestParam("settlementDate")String SettlementDate) throws DateTimeParseException {
-//        // 2테이블에 없는 날짜 내역이 올 경우 에러 -> DateTimeParseException
-//        // 기존 정산 내역에 있는 정산일자를 yyyy-MM 형식으로 바꾸고 storeName을 추가했습니다.
-//        try {
-//            List<SettlementMonth> settlementMonths = settlementMonthService.selectByStoreIdAndDay(1L,SettlementDate);
-//            List<SettlementMonthReportDTO> reportDTOs = new ArrayList<>();
-//
-//            for (SettlementMonth settlementMonth : settlementMonths) {
-//                SettlementMonthReportDTO reportDTO = new SettlementMonthReportDTO();
-//                reportDTO.setSettlementMontnId(settlementMonth.getId());
-//
-//                reportDTO.setSettlementPrice(settlementMonth.getSettlementPrice());
-//                // 월별 정산내역에서 settlementDate를 yyyy-MM-dd에서 yyyy-MM 형식으로 바꾸기 위한 과정 시작
-//                LocalDate localDate = settlementMonth.getSettlementDate();
-//                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM");
-//                String formattDate = localDate.format(dateTimeFormatter);
-//                // 월별 정산내역에서 settlementDate를 yyyy-MM-dd에서 yyyy-MM 형식으로 바꾸기 위한 과정 종료
-//                reportDTO.setSettlementDate(formattDate);
-//                reportDTO.setStoreId(settlementMonth.getStore().getId());
-//                reportDTO.setStoreName(settlementMonth.getStore().getName());
-//                reportDTO.setCreatedDate(settlementMonth.getCreatedDate());
-//                reportDTOs.add(reportDTO);
-//            }
-//            return reportDTOs;
-//        } catch (Exception e) {
-//            return Collections.emptyList();
-//        }
-//    }
-    // 특정 기간 월별 내역 조회
-//    @GetMapping("/settlementMonth-variable-range-yyyy-MM")
-//    public List<SettlementMonthReportDTO> settlementMonthByVariableRangeyyyyMM(@RequestParam("startDate")String startDate, @RequestParam("endDate")String endDate) throws DateTimeParseException {
-//        try {
-//            List<SettlementMonth> settlementMonths = settlementMonthService.selectByStoreIdAndDayBetween(1L, startDate,endDate);
-//            List<SettlementMonthReportDTO> reportDTOs = new ArrayList<>();
-//
-//            for(SettlementMonth settlementMonth: settlementMonths) {
-//                SettlementMonthReportDTO reportDTO = new SettlementMonthReportDTO();
-//                reportDTO.setSettlementMontnId(settlementMonth.getId());
-//                reportDTO.setSettlementPrice(settlementMonth.getSettlementPrice());
-//                LocalDate localDate = settlementMonth.getSettlementDate();
-//                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM");
-//                String formattedDate = localDate.format(dateTimeFormatter);
-//                reportDTO.setSettlementDate(formattedDate);
-//                reportDTO.setStoreId(settlementMonth.getStore().getId());
-//                reportDTO.setStoreName(settlementMonth.getStore().getName());
-//                reportDTO.setCreatedDate(settlementMonth.getCreatedDate());
-//                reportDTOs.add(reportDTO);
-//            }
-//            return reportDTOs;
-//        } catch (Exception e) {
-//            return Collections.emptyList();
-//        }
-//    }
 
     // 월별 기간 상세 조회
     // store_id, "yyyy-mm"
@@ -312,15 +176,15 @@ public class BranchAdminManagerController {
         try {
         OrderDetailResponseDTO orderDetailResponseDTO = new OrderDetailResponseDTO();
         Order order = orderRepository.findBySerialNumber(serialNumber);
+        User user = order.getUser();
         Long orderId = order.getId();
+        Long storeId = order.getPos().getStore().getId();
+        Optional<Store> store = storeRepository.findById(storeId);
         orderDetailResponseDTO.setSerialNumber(serialNumber);
         orderDetailResponseDTO.setOrderDate(order.getOrderDate());
         orderDetailResponseDTO.setTotalPrice(order.getTotalPrice());
         orderDetailResponseDTO.setCouponUsePrice(order.getCouponUsePrice());
-        // 포인트 정보 가져오기 시작
-        Optional<PointUseHistory> pointUseHistory = pointUseHistoryRepository.findByOrderId(orderId);
-        orderDetailResponseDTO.setPointUsePrice(pointUseHistory.get().getPointUseAmount());
-        // 포인트 정보 가져오기 종료
+        orderDetailResponseDTO.setPointUsePrice(order.getPointUsePrice());
         orderDetailResponseDTO.setFinalTotalPrice(order.getFinalTotalPrice());
         List<OrderDetailProductResponseDTO> orderDetailProductResponseDTOList = new ArrayList<>();
         List<Cart> cartList = cartRepository.findAllByOrderId(orderId);
@@ -335,7 +199,22 @@ public class BranchAdminManagerController {
             orderDetailProductResponseDTOList.add(orderDetailProduct);
         }
         orderDetailResponseDTO.setOrderDetailProductResponseDTOList(orderDetailProductResponseDTOList);
-        // 주문 상품의 이름, 수량, 상품 가격 정보 종료
+        if (user == null) {
+            // 사용자가 존재하지 않는 경우
+            orderDetailResponseDTO.setUserName(" ");
+            orderDetailResponseDTO.setUserPoint(0);
+        } else {
+            // 사용자가 존재하는 경우
+            orderDetailResponseDTO.setUserName(user.getName());
+            orderDetailResponseDTO.setUserPoint(user.getPoint().getPointAmount());
+        }
+        int finalTotalPrice = order.getFinalTotalPrice();
+        int productPrice = finalTotalPrice*10/11;
+        int vat = finalTotalPrice*1/11;
+        orderDetailResponseDTO.setProductPrice(productPrice);
+        orderDetailResponseDTO.setVat(vat);
+        orderDetailResponseDTO.setStoreName(store.get().getName());
+        orderDetailResponseDTO.setStoreAdress(store.get().getAddress());
         return new ResponseEntity<>(orderDetailResponseDTO, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
