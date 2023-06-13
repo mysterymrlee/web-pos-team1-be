375172047c9768deb4889ba7b471eda77a2508c5
Kyunghwan Ko <kyunghwanko1207@gmail.com>
2023-06-13 21:40:58 +0900

    chore: add fe link to allow origin

--- src/main/java/com/ssg/webpos/config/WebConfig.java
+++ src/main/java/com/ssg/webpos/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry
                 .addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://3.36.45.238:3000");
+                .allowedOrigins("http://localhost:3000", "http://3.36.45.238:3000", "http://3.34.250.96:3000");
     }
 }
